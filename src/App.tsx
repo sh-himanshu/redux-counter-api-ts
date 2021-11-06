49b6bee1e57f9a3713d692ed474ace865e9d0204
sh-himanshu <88159798+sh-himanshu@users.noreply.github.com>
2021-11-06 16:05:14 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React, { useState } from "react";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+
+import { amountAdded } from "./features/counter/counterSlice";
+import { useFetchBreedsQuery } from "./features/dogs/dogsApiSlice";
+
+const App: React.FC = () => {
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+  const [numDogs, setNumDogs] = useState(10);
+
+  const { data = [], isFetching } = useFetchBreedsQuery(numDogs);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <div className="counter">
+        <h1>Counter</h1>
+        <button onClick={() => dispatch(amountAdded(3))}>Counter is: {count}</button>
+      </div>
+      <hr />
+
+      <div className="dogs">
+        <h1>Dogs</h1>
+        <label htmlFor="dog-select" style={{ marginRight: "15px" }}>
+          Choose Number of Dogs:
+        </label>
+
+        <select
+          name="dog"
+          id="dog-select"
+          value={numDogs}
+          onChange={(e) => setNumDogs(parseInt(e.target.value))}
+        >
+          <option value="0">--Please choose an option--</option>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+        </select>
+        {isFetching ? (
+          <h3>Loading...</h3>
+        ) : (
+          <>
+            <p>Number of dogs fetched: {data.length}</p>
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Picture</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((breed) => (
+                  <tr key={breed.id}>
+                    <td>{breed.name}</td>
+                    <td>
+                      <img src={breed.image.url} alt={breed.name} height="200px" />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,22 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { apiSlice } from "../features/dogs/dogsApiSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(apiSlice.middleware);
   },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+// export type AppThunk<ReturnType = void> = ThunkAction<
+//   ReturnType,
+//   RootState,
+//   unknown,
+//   Action<string>
+// >;
